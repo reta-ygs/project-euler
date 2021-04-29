549d748e91419d5b9e8584de7399ed6089235440
reta <reta428@gmail.com>
2021-04-29 09:59:34 +0900

    prime factor

--- lib/prime-number/src/lib.rs
+++ lib/prime-number/src/lib.rs
@@ -1,12 +1,9 @@
+use std::collections::HashMap;
+
 #[cfg(test)]
 mod tests {
-    use crate::is_prime_number;
+    use crate::{is_prime_number, prime_factors};
     use std::ops::Not;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 
     #[test]
     fn test_is_prime_number() {
@@ -16,13 +13,54 @@ mod tests {
         assert!(is_prime_number(4).not());
         assert!(is_prime_number(19));
     }
+
+    #[test]
+    fn test_prime_factors() {
+        assert_eq!(prime_factors(2), vec![(2, 1)].iter().copied().collect());
+        assert_eq!(prime_factors(4), vec![(2, 2)].iter().copied().collect());
+        assert_eq!(
+            prime_factors(10),
+            vec![(2, 1), (5, 1)].iter().copied().collect()
+        );
+        assert_eq!(
+            prime_factors(20),
+            vec![(2, 2), (5, 1)].iter().copied().collect()
+        );
+    }
 }
 
 pub fn is_prime_number(target: usize) -> bool {
     if target <= 1 {
         return false;
     }
-    (2..(target + 1)).into_iter()
+    (2..=target)
+        .into_iter()
         .filter(|num| num.pow(2) <= target)
         .all(|num| target % num != 0)
+}
+
+pub fn prime_factors(number: usize) -> HashMap<usize, usize> {
+    let mut factors = HashMap::new();
+    let mut remaining = number;
+    for n in (2..=number)
+        .into_iter()
+        .filter(|n| is_prime_number(*n))
+        .filter(|n| number % n == 0)
+    {
+        while remaining % n == 0 {
+            match factors.get_mut(&n) {
+                Some(count) => {
+                    *count += 1;
+                }
+                None => {
+                    factors.insert(n, 1);
+                }
+            }
+            remaining /= n;
+        }
+        if remaining == 1 {
+            break;
+        }
+    }
+    factors
 }
